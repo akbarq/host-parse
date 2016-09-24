fa2195f21e5fad1b58766d5206130884409267d7
akbarq <breakpoint.py@gmail.com>
2016-09-24 12:41:17 -0500

    Update host_parse.py

--- host_parse.py
+++ host_parse.py
@@ -3,21 +3,23 @@
 
 import dpkt
 import sys
+from socket import inet_ntoa as to_ip
 import datetime
+from collections import Counter
 
 if len(sys.argv) < 2:
     sys.exit('Usage: %s pcap-file' % sys.argv[0])
 
 pcap_file = open(sys.argv[1], "r")
 pcap = dpkt.pcap.Reader(pcap_file)
-	     
+
 hosts = []
 for timestamp, buf in pcap:
     eth = dpkt.ethernet.Ethernet(buf)
     ip = eth.data
     tcp = ip.data
     if tcp.__class__.__name__ == 'TCP':
-    	if tcp.dport == 80 and len(tcp.data) > 0:
+        if tcp.dport == 80 and len(tcp.data) > 0:
             try:
                 http = dpkt.http.Request(tcp.data)
                 hosts.append(http.headers['host'])
@@ -26,7 +28,7 @@ for timestamp, buf in pcap:
 
 pcap_file.close()
 
-count = dict((c, hosts.count(c)) for c in set(hosts))
-for key, value in sorted(count.iteritems(), key=lambda (k,v): (v,k),reverse=True):
-    print "\033[1;37;40m Host: %s  Hits: %s\033[0;37;40m" % (key, value)
-	
+c = Counter(hosts).most_common()
+for host,hits in c:
+        print "\033[1;37;40m Host: %s  Hits: %s\033[0;37;40m" % (host,hits)
+
